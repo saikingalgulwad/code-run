3d1798e4fbd3bde061701cdc8523973102cca780
saikingalgulwad <167662574+saikingalgulwad@users.noreply.github.com>
2025-09-18 19:37:42 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,11 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-  <link rel="icon" href="/favicon.ico" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/favicon-32x32.png" />
-  <meta name="theme-color" content="#4F46E5" />
+  <link rel="icon" href="/favicon.svg" />
+  
 </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
