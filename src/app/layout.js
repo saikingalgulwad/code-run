458e4b188f43ce4fd97aadcb42b3491bec8e3557
saikingalgulwad <167662574+saikingalgulwad@users.noreply.github.com>
2025-09-22 14:08:02 +0530

    remove title

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Online Typescript Code Editor | Saiprasad Code",
-  description:
-    "Saiprasad Code is a free online code editor for JavaScript and Python. Write, run, and share your code instantly in a sleek, dark-themed editor designed for developers. Perfect for learning, testing, and building projects online.",
+
   icons: {
     icon: [
       { url: "favicon.svg" },
